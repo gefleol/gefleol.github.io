f4240afbe266ef8dbc53866a9b53a3a13dfad77d
jsr_elite_1 <johan.sanden@gmail.com>
2020-02-07 21:39:51 +0100

    validate is working

--- scripts/validate.py
+++ scripts/validate.py
@@ -1,38 +1,30 @@
 #-*- coding: utf-8 -*-
 from schema import Schema, And, Regex, Optional
 import yaml
+import codecs
 
 if __name__ == '__main__':
 
-    drink_schema = Schema({'name': And(str,len),
-                      Optional('country'):And(str,len),
-                      Optional('brewery'):And(str,len),
-                      Optional('brewer'):And(str,len),
-                      Optional('type'):And(str,len),
+    drink_json_schema = {'name': And(Regex('\w+'),len),
+                      Optional('country'):And(Regex('\w+'),len),
+                      Optional('brewery'):And(Regex('\w+'),len),
+                      Optional('brewer'):And(Regex('\w+'),len),
+                      Optional('type'):And(Regex('\w+'),len),
                       Optional('abv'):And(float,lambda n:0 <= n <=100),
                       Optional('abw'):And(float,lambda n:0 <= n <=100),
                       Optional('ibu'):float,
                       Optional('reviews'):
-                     [{'date': Regex('^\d{4}-\d{2}-\d{2}'),
-                       Optional('group'):And(str,len),
-                       Optional('place'):And(str,len),
-                       'count':int, 
-                       Optional('review_type'):And(float,lambda n:0 <= n <=10), 
-                       Optional('review_over'):And(float,lambda n:0 <= n <=10),
-                       Optional('review_uniq'):And(float,lambda n:0 <= n <=10),
-                       Optional('comments'):[str]}]})
+                         [{'date': Regex('^\d{4}-\d{2}-\d{2}'),
+                           Optional('group'):And(Regex('\w+'),len),
+                           Optional('place'):And(Regex('\w+'),len),
+                           'count':int, 
+                           Optional('review_type'):And(float,lambda n:0 <= n <=10), 
+                           Optional('review_over'):And(float,lambda n:0 <= n <=10),
+                           Optional('review_uniq'):And(float,lambda n:0 <= n <=10),
+                           Optional('comments'):[Regex('\w+')]}]}
 
-    document="""name: "Odd Island Brew"
-country: "Sverige"
-brewery: "Hazie Dizzie"
-type: "Ale"
-abv: 6.2
-reviews: 
-    - date: "2020-01-02"
-      group: "Gefle Olsellskap"
-      place: "Terrassen"
-      count: 5
-      review_type: 8.3 
-      comments:
-        - God ol/Smakvik"""
-    print drink_schema.validate(yaml.load(document))
+    drink_schema=Schema(drink_json_schema)
+    with codecs.open('../data/drinks/odd_island_brew-hazie_dizzie.yaml', 'rb', encoding='utf-8') as s:
+        stream=s.read()
+        document=yaml.load(stream,Loader=yaml.FullLoader)
+        drink_schema.validate(document)
