d16deee228aaac7f609620197ffa993cce994854
jsr_elite_1 <johan.sanden@gmail.com>
2020-02-09 22:16:17 +0100

    remove underscore

--- src/create_org_doc.py
+++ src/create_org_doc.py
@@ -1,9 +1,9 @@
 import directoryparser as dir
 import drinks_parser as dp
+from document.document import Document
 
-
-drink_directory="/home/johan/dev/gefleol.github.io/data/drinks/"
-org_content=""
+drink_directory = "/home/johan/dev/gefleol.github.io/data/drinks/"
+pub_directory = "/home/johan/dev/gefleol.github.io/pub/"
 print_dict=dict()
 docs=[]
 
@@ -11,18 +11,9 @@ if __name__ == '__main__':
     for drink_file in dir.get_drink_files(drink_directory):
         docs.append(dp.get_drink_dict(drink_file))
 
-# {"country": {"brewery":[{drink_name:doc}]}}
+    # {"country": {"brewery":[{drink_name:doc}]}}
     print_dict=dp.order_drinks(docs)
-        
-    for country in print_dict.keys():
-        org_content+="** {}\n".format(country.encode("utf-8"))
-        for brewery in print_dict[country]:
-            org_content+="*** {}\n".format(brewery.encode("utf-8"))
-            for drink in print_dict[country][brewery]:
-                org_content+="**** {}\n".format(drink["name"].encode("utf-8"))
-                for review in drink["reviews"]:
-                    org_content+="    + Provdatum: {}\n".format(review["date"])
-                    # org_content+="        - Land: {}\n".format(re)
-    print org_content
-            
+    doc=Document(pub_directory)
+    doc.create_content(print_dict)
+    doc.save_basic_file("test.org")
     
